256148647b166859a721741cfc4b310baf9b4d01
Ryan Gates <Ryan_Gates@ultimatesoftware.com>
2017-03-30 16:38:25 -0400

    Added global confirmation
    Added git and java runtime

--- Jenkins/Jenkins_Slave_Prerequisites.ps1
+++ Jenkins/Jenkins_Slave_Prerequisites.ps1
@@ -1,5 +1,10 @@
 Set-ExecutionPolicy Bypass -Force
 
+choco feature enable -n allowGlobalConfirmation
+
 # Don't forget to ensure ExecutionPolicy above
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+choco install javaruntime
+
+choco install git
